e0af69b47f08e47d65392e6bb3ee1ee776ee2de5
Tal Zussman <talzuss@gmail.com>
2021-08-09 16:51:07 +0300

    Comment out parse_keys

--- rabpro/utils.py
+++ rabpro/utils.py
@@ -162,30 +162,30 @@ def get_exportpaths(name, basepath=None, overwrite=False):
     return exportpaths
 
 
-def parse_keys(gdf):
-    """ 
-    Attempts to interpret the column names of the input dataframe.
-    In particular, looks for widths and distances along centerline.
+# def parse_keys(gdf): # Used only in centerline, deprecated
+#     """ 
+#     Attempts to interpret the column names of the input dataframe.
+#     In particular, looks for widths and distances along centerline.
 
-    Parameters
-    ----------
-    gdf : GeoDataFrame
-        table to parse
+#     Parameters
+#     ----------
+#     gdf : GeoDataFrame
+#         table to parse
 
-    Returns
-    -------
-    dict
-        contains column names and corresponding properties
-    """
-    keys = gdf.keys()
-    parsed = {"distance": None, "width": None}
-    for k in keys:
-        if "distance" in k.lower():
-            parsed["distance"] = k
-        if "width" in k.lower():
-            parsed["width"] = k
+#     Returns
+#     -------
+#     dict
+#         contains column names and corresponding properties
+#     """
+#     keys = gdf.keys()
+#     parsed = {"distance": None, "width": None}
+#     for k in keys:
+#         if "distance" in k.lower():
+#             parsed["distance"] = k
+#         if "width" in k.lower():
+#             parsed["width"] = k
 
-    return parsed
+#     return parsed
 
 
 def build_vrt(
