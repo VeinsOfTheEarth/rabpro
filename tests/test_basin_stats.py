3bbf65bcceb959750b321d8b5f13677c49370483
jsta <stachel2@msu.edu>
2021-09-28 08:37:36 -0600

    add imagecollection tests

--- tests/test_basin_stats.py
+++ tests/test_basin_stats.py
@@ -5,11 +5,56 @@ from shapely.geometry import box
 import pandas as pd
 
 
-def test_basin_stats():
-    coords_file = gpd.read_file(r"tests/data/Big Blue River.geojson")
-    gdf = gpd.GeoDataFrame(
-        {"idx": [1], "geometry": [box(*coords_file.total_bounds)]}, crs="EPSG:4326"
+coords_file = gpd.read_file(r"tests/data/Big Blue River.geojson")
+gdf = gpd.GeoDataFrame(
+    {"idx": [1], "geometry": [box(*coords_file.total_bounds)]}, crs="EPSG:4326"
+)
+
+
+def clean_res(feature):
+    res = pd.DataFrame(feature["properties"], index=[0])
+    res["id"] = feature["id"]
+    return res
+
+
+def test_timeindexed_imgcol():
+
+    data, task = rabpro.subbasin_stats.main(
+        gdf,
+        [
+            Dataset(
+                "JRC/GSW1_3/YearlyHistory",
+                "waterClass",
+            )
+        ],
+        test=True,
     )
+
+    res = pd.concat([clean_res(feature) for feature in data["features"]])
+
+    assert res["mean"].iloc[0] > 0
+    assert res.shape[0] > 0
+
+
+def test_nontimeindexed_imgcol():
+
+    data, task = rabpro.subbasin_stats.main(
+        gdf,
+        [
+            Dataset(
+                "JRC/GSW1_3/MonthlyRecurrence",
+                "monthly_recurrence",
+            )
+        ],
+        test=True,
+    )
+
+    res = pd.concat([clean_res(feature) for feature in data["features"]])
+
+    assert res.shape[0] > 0
+
+
+def test_img():
 
     data, task = rabpro.subbasin_stats.main(
         gdf,
