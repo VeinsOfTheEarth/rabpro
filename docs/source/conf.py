37d3d93beb49907e2d1d664bf7ba7c12dc0c4a8a
jsta <stachel2@msu.edu>
2022-02-09 16:44:42 -0700

    make user dataset table scrollable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -120,4 +120,9 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["../_static"]
 
+html_css_files = [
+    "css/custom.css",
+]
+
+
 html_logo = "logo_small.png"
