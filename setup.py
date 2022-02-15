b85aeab26905f8d3276d42cd23392a7240d49bd6
jsta <stachel2@msu.edu>
2022-02-15 09:39:42 -0700

    increment for pre-joss release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rabpro",
-    version="0.3.0",
+    version="0.3.1",
     author="Example Author",
     author_email="author@example.com",
     description="Package to delineate subbasins and compute statistics.",
