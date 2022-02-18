0c1dac7460bd5ae81a7dd1bc9a49396d9f1a2d1b
jsta <stachel2@msu.edu>
2022-02-18 15:58:35 -0700

    stash

---
+++ docs/paper_fig/generate_basin_stats_files.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import geopandas as gpd
+
+import rabpro
+from rabpro.basin_stats import Dataset
+
+import seaborn as sns
+
+path_base = "docs/paper_fig/"
+basin = gpd.read_file(path_base + "basin_merit.gpkg")
+basin["id"] = 0
+
+dataset_list = [
+    Dataset(
+        "ECMWF/ERA5_LAND/MONTHLY",
+        "temperature_2m",
+        stats=["mean"],
+        start="2000-06-01",
+        end="2021-09-01",
+    ),
+    Dataset(
+        "NASA/GPM_L3/IMERG_MONTHLY_V06",
+        "precipitation",
+        stats=["mean"],
+        start="2000-06-01",
+        end="2021-09-01",
+    ),
+]
+
+
+urls, tasks = rabpro.basin_stats.compute(
+    dataset_list, basins_gdf=basin, folder="rabpro"
+)
+
+tag_list = ["temperature", "precip"]
+data = rabpro.basin_stats.fetch_gee(urls, tag_list)
+
+# ---- format time column
+test = data.copy()
+test = test.rename(columns={"temperature_system:index": "date"}).head()
+test = test.assign(
+    year=lambda x: x.date.str.slice(0, 4),
+    month=lambda x: x.date.str.slice(4, 6),
+    day="01",
+)
+date = pd.to_datetime(test[["year", "month", "day"]])
+test["date"] = date
+test = test.drop(["year", "month", "day"], axis=1).filter(regex="^((?!system).)*$")
+
+# ----
+
+sns.lineplot(x="date", y="precip_mean", data=data)
+
+res = gpd.GeoDataFrame(data.merge(basin, on="id"))
+res = res.set_geometry("geometry")
+res.to_file("res.gpkg", driver="GPKG")
+
