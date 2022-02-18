14429fc65ce4cd9614443d20f53ffcd77660c621
jsta <stachel2@msu.edu>
2022-02-18 16:42:32 -0700

    stash progress

--- docs/paper_fig/generate_basin_stats_files.py
+++ docs/paper_fig/generate_basin_stats_files.py
@@ -35,21 +35,33 @@ urls, tasks = rabpro.basin_stats.compute(
 tag_list = ["temperature", "precip"]
 data = rabpro.basin_stats.fetch_gee(urls, tag_list)
 
-# ---- format time column
-test = data.copy()
-test = test.rename(columns={"temperature_system:index": "date"}).head()
-test = test.assign(
+# format time column
+data = data.rename(columns={"temperature_system:index": "date"}).assign(
     year=lambda x: x.date.str.slice(0, 4),
     month=lambda x: x.date.str.slice(4, 6),
     day="01",
 )
-date = pd.to_datetime(test[["year", "month", "day"]])
-test["date"] = date
-test = test.drop(["year", "month", "day"], axis=1).filter(regex="^((?!system).)*$")
+date_column = pd.to_datetime(data[["year", "month", "day"]])
+data["date"] = date_column
+data = data.drop(["year", "month", "day"], axis=1).filter(regex="^((?!system).)*$")
+
+data.to_csv("test.csv", index=False)
 
 # ----
 
-sns.lineplot(x="date", y="precip_mean", data=data)
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
+
+data = pd.read_csv("test.csv")
+
+f, axs = plt.subplots(1, 1)
+sns.lineplot(x="date", y="precip_mean", data=data, ax=axs)
+date_form = DateFormatter("%Y-%m")
+axs.xaxis.set_major_formatter(date_form)
+axs.xaxis.set_major_locator(mdates.YearLocator())
+plt.show()
+
 
 res = gpd.GeoDataFrame(data.merge(basin, on="id"))
 res = res.set_geometry("geometry")
