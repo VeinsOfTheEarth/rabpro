049e6231a0a54a375b7dd34a9f365f242d6b083a
Jemma Stachelek <jemma.stachelek@gmail.com>
2024-01-22 14:47:08 -0700

    formatting

--- Data/webparser.py
+++ Data/webparser.py
@@ -11,7 +11,9 @@ CATALOG_URL = "https://earthengine-stac.storage.googleapis.com/catalog/catalog.j
 
 
 def parse_date(timestamp):
-    return datetime.fromtimestamp(timestamp / 1000, tz=timezone.utc).strftime("%Y-%m-%d")
+    return datetime.fromtimestamp(timestamp / 1000, tz=timezone.utc).strftime(
+        "%Y-%m-%d"
+    )
 
 
 def parse_url(url, deprecated, verbose):
@@ -22,21 +24,20 @@ def parse_url(url, deprecated, verbose):
             return None
 
         r = response.json()
-        
+
         # Added by JS to account for an apparently new Catalog structure?
         # The catalog urls no longer point directly to the asset metadata
         # url, but must be unpacked. It is possible that multiple assets be
         # contained in the same Catalog entry, so must loop over all 'child'
         # assets using their direct url.
         direct_url = []
-        for link in r['links']:
-            if 'rel' in link:
-                if link['rel'] == 'child':
-                    direct_url.append(link['href'])
-        
-        assets = [] # for storing assets
+        for link in r["links"]:
+            if "rel" in link:
+                if link["rel"] == "child":
+                    direct_url.append(link["href"])
+
+        assets = []  # for storing assets
         for durl in direct_url:
-            
             response = requests.get(durl)
             if response.status_code != 200:
                 print(f"{url} returned error status code {response.status_code}")
@@ -46,12 +47,12 @@ def parse_url(url, deprecated, verbose):
             gee_id = r["id"]
             gee_name = r["title"]
             gee_type = r["gee:type"]
-    
+
             if not deprecated and "deprecated" in r and r["deprecated"]:
                 if verbose:
                     print(f"Skipping {gee_id} (deprecated)")
                 continue
-    
+
             # Ignoring anything that's not an Image or ImageCollection
             # i.e. skipping featureCollections
             if gee_type == "image_collection":
@@ -62,7 +63,7 @@ def parse_url(url, deprecated, verbose):
                 if verbose:
                     print(f"Skipping {gee_id} (not an Image or ImageCollection)")
                 continue
-    
+
             # Get date range information
             try:
                 start_timestamp, end_timestamp = img.get("date_range").getInfo()
@@ -72,7 +73,7 @@ def parse_url(url, deprecated, verbose):
                 # Fallback method to compute the date range
                 if verbose:
                     print(f"Fallback date method for {gee_id}")
-    
+
                 gee_start = r["extent"]["temporal"]["interval"][0][0].split("T")[0]
                 if r["extent"]["temporal"]["interval"][0][1] is not None:
                     gee_end = r["extent"]["temporal"]["interval"][0][1].split("T")[0]
@@ -80,7 +81,7 @@ def parse_url(url, deprecated, verbose):
                     if verbose:
                         print(f"No end date found for {gee_id}. Using today's date.")
                     gee_end = datetime.now().strftime("%Y-%m-%d")
-    
+
             if gee_type == "image_collection":
                 l = img.toList(2)
                 if img.toList(2).length().getInfo() > 1:
@@ -89,9 +90,9 @@ def parse_url(url, deprecated, verbose):
                     img = ee.Image(l.get(1))
                 else:
                     img = img.first()
-    
+
             # Get band, units, and resolution information
-    
+
             # Account for COPERNICUS/S1_GRD edge case with "sar:bands"
             bandkey = "sar:bands" if "eo:bands" not in r["summaries"] else "eo:bands"
             bands = r["summaries"][bandkey]
@@ -99,16 +100,18 @@ def parse_url(url, deprecated, verbose):
             for band in bands:
                 unit = band["gee:unit"] if "gee:unit" in band else None
                 gee_bands[band["name"]] = {"units": unit}
-    
+
             try:
                 bandslist = ee.List(list(gee_bands.keys()))
-                get_resolution = lambda b: ee.List([b, img.select([b]).projection().nominalScale()])
+                get_resolution = lambda b: ee.List(
+                    [b, img.select([b]).projection().nominalScale()]
+                )
                 resolutions = bandslist.map(get_resolution).getInfo()
                 for band, resolution in resolutions:
                     gee_bands[band]["resolution"] = resolution
             except Exception as e:
                 print(e, gee_id)
-    
+
             asset = {
                 "id": gee_id,
                 "name": gee_name,
@@ -119,7 +122,7 @@ def parse_url(url, deprecated, verbose):
             }
             assets.append(asset)
         return assets
-    
+
     except Exception as e:
         print("Error:", e, gee_id)
         return None
@@ -139,13 +142,14 @@ def ee_catalog(deprecated, verbose):
         except Exception as e:
             print(e)
 
-    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "gee_datasets.json")
+    filepath = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "gee_datasets.json"
+    )
     with open(filepath, "w") as f:
         json.dump(catalog, f, indent=4)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="Parse GEE Data Catalog metadata")
 
     parser.add_argument(
